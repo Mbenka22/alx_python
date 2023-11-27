c6449424dec699f47d8a934aca5bb977d81aad11
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-11-27 23:02:59 +0300

    1-edges

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -2,14 +2,19 @@
 word="Holberton"
 substring= word[:3]
 print(substring)
+print()
 #word_last_2 should contain the last 2 letters of the variable word
 word="Holberton"
-substring=word[-2]
+substring=word[-2:]
 print(substring)
+print()
+print()
 #middle_word should contain the value of the variable word without the first and last letters
 word="Holberton"
-middle_word=word[1:-1]
-print(middle_word)
+substring=word[1:-1]
+print(substring)
+print()
+print()
 
 
 
