d61212a9e6cdb4f72c10c1a70d530c1576f89031
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-11-28 12:39:52 +0300

    format

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,3 +1,3 @@
 for i in range (10):
     for j in range(i,10):
-        print(f"{i:02d},{j:02d}" if i !=j else "", end=", "if j<9 else "\n")
+        print(f"{i:02d},{j:02d}" if i!=j else "", end=", "if j<9 else "\n".format(i))
