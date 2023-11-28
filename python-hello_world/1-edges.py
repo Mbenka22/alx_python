fd3b1ef2975e7f12bf71c1e2aea0ab8e8b6b00fd
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-11-28 23:07:22 +0300

    holberton

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,21 +1,7 @@
-#word_first_3 should contain the first 3 letters of the variable word
 word="Holberton"
-substring= word[:3]
-print(substring)
-print()
-#word_last_2 should contain the last 2 letters of the variable word
-word="Holberton"
-substring=word[-2:]
-print(substring)
-print()
-print()
-#middle_word should contain the value of the variable word without the first and last letters
-word="Holberton"
-substring=word[1:-1]
-print(substring)
-print()
-print()
-
+print("first 3 letters:{}".format(word [0:3]))
+print("last 2 letters: {}".format(word[-2:]))
+print("middle word:{}".format(word[1:-1]))
 
 
 
