8e487d8d61074fd82f79c5ad6b238b47c2956688
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-11-29 02:07:04 +0300

    puzzle

--- python-hello_world/0-print.py
+++ python-hello_world/0-print.py
@@ -1,5 +1,6 @@
-#to print the first python command with a new line at the end
-print ('"Programming is like building a multilingual puzzle')
+
+print ('"Programming is like building a multilingual puzzle ')
 print()
+
 
 
