a16c1b10bea89ba5cb23b4398c6b70a64edff508
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-11-29 10:56:02 +0300

    commit

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,5 +1,5 @@
 import random
-number= random.randint(-10,10)
+number= random.randint(-10, 10)
 if number > 0:
     print("{} is positive".format(number))
 elif number < 0:
--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,5 +1,5 @@
 for i in range (10):
-    for j in range(i,10):
-    print(f"{i:02d}", end=)
+    for j in range( 1+1,10):
+      print("{:02}".format(i=10+j), end=", " if (i!=8 or j!=9)else "\n")
  
         
