f62299cf3978de1fc0eea406c41605b164f59014
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-11-30 12:27:22 +0300

    true

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -18,6 +18,6 @@ def validate_password(password):
     # Password meets all conditions
     return True
 print(validate_password("Password123"))
-print(validate_password("abc123"))
-print(validate_password("Password 123"))
-print(validate_password("password123"))
+#print(validate_password("abc123"))
+#print(validate_password("Password 123"))
+#print(validate_password("password123"))
