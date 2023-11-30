d30c4a03061ecc73a881bdf421669c09edd2d57c
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-11-30 10:47:12 +0300

    on

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -6,17 +6,3 @@ middle_word = word[1:-1]
 print(word_first_3)
 print(word_last_2)
 print(middle_word)
-print(word_first_3, word_last_2, middle_word)
-print(word_first_3 + word_last_2 + middle_word)
-print(f"{word_first_3}{word_last_2}{middle_word}")
-print(" ".join([word_first_3, word_last_2, middle_word]))
-print(f"{word_first_3} {word_last_2} {middle_word}")
-
-
-
-
-
-
-
-
-
