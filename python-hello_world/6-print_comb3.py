21cb182ca735f37e582ce6adfc29a4660c132707
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-11-30 10:22:54 +0300

    with format

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,3 +1,3 @@
-for i in range(0, 10):
+for i in range(10):
     for j in range(i + 0, 10):
-        print(f"{i}{j}", end=", " if i != 9 or j != 8 else "\n")      
+        print(f"{i}{j}", end=", " if i != 8 or j != 9 else "\n".format(i))     
