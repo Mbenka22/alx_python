2693afe2988caa42ba5732bbb8e53f1f928aafb2
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-11-30 13:18:11 +0300

    Holberton

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,7 +1,8 @@
-word= "Holberton"
-word_first_3=word[0:3]
-word_last_2=word[-2:]
-middle_word=word[1:-1]
+word="Holberton"
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word = word[1:-1]
+
 
 
 
