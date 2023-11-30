b29035d1377aca6bdbb0724e28a729c408064dfa
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-11-30 10:51:18 +0300

    3

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,4 +1,8 @@
-def add(a,b):
-    return a+b
-print(add(1, 2))
-
+def add(a, b):
+    # Using bitwise operations to simulate addition
+    while b != 0:
+        carry = a & b
+        a = a ^ b
+        b = carry << 1
+    return a
+print (add(1,2))
