73bd583116d0f209bfa81d4342970d4fd412cbe7
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-12-04 14:24:43 +0300

    argv

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,6 @@
+if __name__=="__main__":
+    from args_1 import myarguments
+
+    result = myarguments()
+    if results:
+        print(result)
---
+++ python-import_modules/args_1.py
@@ -0,0 +1,15 @@
+#!usr/bin/python3
+def myarguments():
+    import sys
+    arguments=sys.argv[1:]
+    num_arguments=len(arguments)
+    output=""
+    if num-arguments==0:
+        output +="0 argument{}.\n".format('s' if num_arguments !=1 else '')
+    else:
+        output +="{} argument{}:\n".format(num_arguments, 's' if num_arguments !=1 else '')   
+
+        # Construct string for each argument with its place
+        for i , arg in enumerate (arguments,1):
+            output+="{}:{}\n".format(i,arg)
+        return output.strip()    
