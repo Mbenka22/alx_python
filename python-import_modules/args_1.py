f6b6a9d0adf757d738134ebaa4dc265fe0a27721
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-12-04 14:26:16 +0300

    argvs

--- python-import_modules/args_1.py
+++ python-import_modules/args_1.py
@@ -4,7 +4,7 @@ def myarguments():
     arguments=sys.argv[1:]
     num_arguments=len(arguments)
     output=""
-    if num-arguments==0:
+    if num_arguments==0:
         output +="0 argument{}.\n".format('s' if num_arguments !=1 else '')
     else:
         output +="{} argument{}:\n".format(num_arguments, 's' if num_arguments !=1 else '')   
