84e10429488f34b6693f2116ea46e7aac5c6294f
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-12-04 12:47:11 +0300

    name-

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,11 +1,8 @@
-# Define variables a and b
 if __name__=="_main_":
     a=1
     b=2
+from add_0 import add
 
-    #import the add function
-    from add_0 import add
-
-    #perform the addition and print the result
-    result=add(a,b)
-    print("{}+{}={}".format(a,b,result))
+result=add(a,b)
+print("{}+{}={}").format(a,b,result)
+      
