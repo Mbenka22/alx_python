5cc7995e4485e15bb7f26cffac8ea2555ee6c7d5
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-12-04 14:37:26 +0300

    0 arguments

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,6 +1,8 @@
-if __name__=="__main__":
-    from args_1 import myarguments
+from sys import argv
 
-    result = myarguments()
-    if results:
-        print(result)
+if __name__ == '__main__':
+    num_args = len(argv) - 1  # Subtract 1 to exclude the script name
+    print(f"{num_args} {'argument' if num_args == 1 else 'arguments'}{'.' if num_args == 0 else ':'}")
+
+    for i in range(1, num_args + 1):
+        print(f"{i}: {argv[i]}")
