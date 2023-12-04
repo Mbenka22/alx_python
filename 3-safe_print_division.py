6d6cc20d10cb7f9888b2e70bf03b6808491070ef
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-12-04 15:04:50 +0300

    file exists

--- 3-safe_print_division.py
+++ 3-safe_print_division.py
@@ -1,17 +1,19 @@
 def safe_print_division(a, b):
     a=12
     b=2
-    try:
-        result = a / b
-    except ZeroDivisionError:
-        return None
-    else:
-        return result
-    finally:
-        print("Inside result: {}".format(result))
+    #!/usr/bin/python3
+safe_print_division = __import__('3-safe_print_division').safe_print_division
 
-# Test cases
-result = safe_print_division(12, 2)
+a = 12
+b = 2
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
+
+a = 12
+b = 0
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
+
 print("Result:", result)
 
 result = safe_print_division(12, 2)
