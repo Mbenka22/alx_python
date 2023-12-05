23c89a8198ba06c5b6e4123f9e2c3bdbfa23bff7
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-12-05 11:36:39 +0300

    matrix

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -3,12 +3,4 @@ def print_matrix_integer(matrix):
         for item in row:
             print(item, end=' ')
         print()
-
-# Test the function with the provided matrix
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix)
+        
