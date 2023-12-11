56e8307b7f8a87c61ff8a43a96c8ada945d7aaaa
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-12-11 14:47:58 +0300

    square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,15 +1,24 @@
-class Square :
-    '''This is a class square which has a myriad of parameters and functions such as the height , width amd the area
-    '''
+"""Module to define a Square class"""
+
+class Square:
+    """Represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
 
     def __init__(self, size):
         self.__size = size
 
     def area(self):
+        """Calculate the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
         return self.__size ** 2
 
 # Example usage:
-# Create a square with size 5
+# Create a square with size 
 my_square = Square()
-print("Area of the square:", my_square.area())  # Output: Area of the square: 25
-
+print("Area of the square:", my_square.area())  # Output: Area of the square: 
