d67d8df7d5781498611eb3b576d7a9284cd3454a
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-12-11 16:57:33 +0300

    square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,16 +1,22 @@
+'''This module will explore the class square'''
 class Square:
     def __init__(self, size):
-        self.__size = size
+        self.size = size
 
-    def get_size(self):
-        return self.__size
+        '''this function will be used to define the square '''
 
-    def set_size(self, size):
-        self.__size = size
+    def set_size(self, new_size):
+        self.size = new_size
+        '''I will use the set function to set the parameters of the square'''
 
-# Example usage:
-my_square = Square(3)
-print("Size of the square:", my_square.get_size())  # Output: Size of the square: 
+    def get_area(self):
+        return self.size ** 2
 
-my_square.set_size(3)
-print("Updated size of the square:", my_square.get_size())  # Output: Updated size of the square: 
+
+# Create an instance of the Square class
+square = Square()
+
+# Get the area of the square
+area = square.get_area()
+print("Area of the square:", area)  
+
