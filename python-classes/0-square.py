feba80f7965ba6053dde1cf34702c4512fe240da
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-12-11 17:04:55 +0300

    0-square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,13 +1,17 @@
+'''the module will define the class square'''
 class Square:
+    '''initializing size'''
     def __init__(self, size):
         self.__size = size
 
 # Test cases
 if __name__ == "__main__":
     my_square = Square(3)
-    print(type(my_square))  # Output: <class '__main__.Square'>
-    print(my_square.__dict__)  # Output: {'_Square__size': 3}
-
+    '''Output:class '__main__.Square'''
+    print(type(my_square))  
+    
+    '''Output: {'_Square__size': 3}'''
+    print(my_square.__dict__)  
     try:
         print(my_square.size)  # Trying to access private attribute directly raises an AttributeError
     except AttributeError as e:
