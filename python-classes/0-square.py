ffe8cca9d0588754530289a7b50270599431d30d
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-12-11 14:57:47 +0300

    Square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -9,31 +9,30 @@ my_square_1 = Square(3)
 print(type(my_square_1))
 print(my_square_1.__dict__)
 
-my_square_2 = Square()  # Instantiating with default size
+my_square_2 = Square()
 print(type(my_square_2))
-print(my_square_2.__dict__)  # This will show an empty dictionary as size is initialized with 0
+print(my_square_2.__dict__)
 
 try:
-    print(my_square_1.size)  # AttributeError as size attribute is private
+    print(my_square_1.size)
 except Exception as e:
     print(e)
 
 try:
-    print(my_square_1.__size)  # AttributeError as __size attribute is private
+    print(my_square_1.__size)
 except Exception as e:
     print(e)
 
 try:
-    my_square_3 = Square("3")  # TypeError as "3" is not an integer
+    my_square_3 = Square("3")
     print(type(my_square_3))
     print(my_square_3.__dict__)
 except Exception as e:
     print(e)
 
 try:
-    my_square_4 = Square(-89)  # Instantiation with negative size defaults to 0
+    my_square_4 = Square(-89)
     print(type(my_square_4))
     print(my_square_4.__dict__)
 except Exception as e:
     print(e)
-
