372179ff8b9ccc009353c94e840870523f613042
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-12-13 11:39:18 +0300

    BaseGeometry

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -2,9 +2,14 @@ class BaseGeometry:
     pass
 bg = BaseGeometry()
 print(bg)
-print()
+def is_same_attributes(obj):
+    expected_attributes = []
+    obj_attributes = dir(obj)
+    return obj_attributes == expected_attributes
+   
+
 print(dir(bg))
-print()
+
 print(dir(BaseGeometry))
 
 
