bf287a82aa3db782fa2dff33edd621bb4e16dd7f
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-12-14 14:01:10 +0300

    full rectangle

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,5 +1,6 @@
-BaseGeometry = __import__("5-base_geometry").BaseGeometry
 '''The module will import some functionalities from another module'''
+BaseGeometry = __import__("5-base_geometry").BaseGeometry
+'''A class that has inherited from Basegeometry'''
 class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         self.__width = 0
@@ -12,7 +13,7 @@ class Rectangle(BaseGeometry):
         '''The function will find the area of the rectangle'''
     def area(self):
          return self.__width * self.__height
-         '''The string function will be introduced'''
+        
     def __str__(self):
          return f"Rectangle with width={self.__width} and height={self.__height}"
     
