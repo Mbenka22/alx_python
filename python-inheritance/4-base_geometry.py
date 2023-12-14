2f0bd687800dfd3aec2e4b9f290f4721045fc150
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-12-14 12:58:05 +0300

    4improve geo

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,15 +1,5 @@
-''' Instantiate the class'''
+'''The class base geometry'''
 class BaseGeometry:
-
+    '''the function for finding the area'''
     def area(self):
-        '''Try calling the area method and catch the raised exception'''
         raise Exception("area() is not implemented")
-
-
-#bg = BaseGeometry()
-
-
-# try:
-#     print(bg.area())
-# except Exception as e:
-#     print("[{}] {}".format(e.__class__.__name__, e))
