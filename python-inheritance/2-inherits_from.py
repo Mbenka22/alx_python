23bea19646adfeb3db77cba0f3f0ef8af14d81c1
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-12-14 12:27:23 +0300

    inherits

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -6,9 +6,9 @@ def inherits_from(obj, a_class):
     ''' Check if any class in the object's method resolution order is a subclass of a_class'''
     for cls in obj_classes:
         if cls is a_class or issubclass(cls, a_class):
-            return True
+            return False
     
-    return False
+    return True
 
 
 # a = True
