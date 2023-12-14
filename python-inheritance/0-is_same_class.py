7ee35a4dda091b45cdc1b66fc2a80535b93716c6
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-12-14 12:12:37 +0300

    inherits

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -2,7 +2,7 @@
 def is_same_class(obj, a_class):
 
     '''check to see if it is true it is an interger or not'''
-    return type(obj) == a_class
+    return type(obj) == a_class 
 
 
 
--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,6 +1,6 @@
-
+''' Get the classes the object is an instance of'''
 def inherits_from(obj, a_class):
-    ''' Get the classes the object is an instance of'''
+    
     obj_classes = type(obj).__mro__
     
     ''' Check if any class in the object's method resolution order is a subclass of a_class'''
