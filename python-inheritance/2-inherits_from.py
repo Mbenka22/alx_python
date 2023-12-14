af730bc33532f6718f0143c63c527220f397fca1
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-12-14 12:06:57 +0300

    inherits

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,19 +1,12 @@
 
-def is_kind_of_class(obj, a_class):
-    '''Check if the object's class matches the specified class'''
-    if type(obj) is a_class:
-        return True
+def inherits_from(obj, a_class):
+    ''' Get the classes the object is an instance of'''
+    obj_classes = type(obj).__mro__
     
-    '''Check if the object's class inherits from the specified class'''
-    for base_class in type(obj).__bases__:
-        if base_class is a_class or is_kind_of_class(base_class, a_class):
+    ''' Check if any class in the object's method resolution order is a subclass of a_class'''
+    for cls in obj_classes:
+        if cls is a_class or issubclass(cls, a_class):
             return True
     
     return False
-# a = 1
-# if is_kind_of_class(a, int):
-#     print("{} comes from {}".format(a, int.__name__))
-# if is_kind_of_class(a, float):
-#     print("{} comes from {}".format(a, float.__name__))
-# if is_kind_of_class(a, object):
-#     print("{} comes from {}".format(a, object.__name__))
+
