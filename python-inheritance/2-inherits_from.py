1359f38bd9d3bc16798fd313e34f33cfab15a8ee
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-12-14 12:02:59 +0300

    inherits

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,10 +1,10 @@
 
 def is_kind_of_class(obj, a_class):
-    # Check if the object's class matches the specified class
+    '''Check if the object's class matches the specified class'''
     if type(obj) is a_class:
         return True
     
-    # Check if the object's class inherits from the specified class
+    '''Check if the object's class inherits from the specified class'''
     for base_class in type(obj).__bases__:
         if base_class is a_class or is_kind_of_class(base_class, a_class):
             return True
