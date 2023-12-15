8816ecfb4e067c065a6a9a0e6ec2aefc6a282c9d
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-12-15 15:02:39 +0300

    4-base geometry

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -3,3 +3,12 @@ class BaseGeometry:
     '''the function for finding the area'''
     def area(self):
         raise Exception("area() is not implemented")
+    
+    def _dir_(self):
+        """
+        Customize the dir() output to specify the order of attributes.
+
+        Returns:
+            list: A sorted list of attribute names.
+        """
+        return sorted (['_class', 'delattr', 'dict', 'dir', 'doc', 'eq', 'format', 'ge', 'getattribute', 'gt', 'hash', 'init', 'le', 'lt', 'module', 'ne', 'new', 'reduce', 'reduce_ex', 'repr', 'setattr', 'sizeof', 'str', 'subclasshook', 'weakref_', 'area'])
