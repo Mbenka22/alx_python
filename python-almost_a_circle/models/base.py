976ce405231e2db47699254ce6f3bf9bc851a365
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-12-17 00:15:38 +0300

    base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -12,6 +12,17 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 '''the test code'''
-# b1 = Base()
-# print(b1.id)
+b1 = Base()
+print(b1.id)
 
+b2 = Base()
+print(b2.id)
+
+b3 = Base()
+print(b3.id)
+
+b4 = Base(12)
+print(b4.id)
+
+b5 = Base()
+print(b5.id)
