b170d1e31c546955a495cf01e5fe28c196cae071
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-12-17 12:27:39 +0300

    Reactangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -38,13 +38,14 @@ class Rectangle(Base):
 
     def set_y(self, y):
         self.__y = y
+
 # if __name__ == "__main__":
 
-    # r1 = Rectangle(10, 2)
-    # print(r1.id)
+#     r1 = Rectangle(10, 2)
+#     print(r1.id)
 
-    # r2 = Rectangle(2, 10)
-    # print(r2.id)
+#     r2 = Rectangle(2, 10)
+#     print(r2.id)
 
-    # r3 = Rectangle(10, 2, 0, 0, 12)
-    # print(r3.id)
+#     r3 = Rectangle(10, 2, 0, 0, 12)
+#     print(r3.id)
