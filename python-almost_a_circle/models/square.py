c0b1dcd0d78cc27ab328bf72177d3d8278d87206
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-12-18 18:21:21 +0300

    square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -4,6 +4,10 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     '''Initializes a Square instance.'''
     def __init__(self, size, x=0, y=0, id=None):
+        self.size = size
+        self.x = x
+        self.y = y 
+        self.id = id
         '''
         Initializes a Square instance.'''
 
@@ -20,52 +24,25 @@ class Square(Rectangle):
         """
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
     
-    @property
-    def width(self):
-        return self._width
+    
+    
+if __name__ == "__main__":
 
-    @width.setter
-    def width(self, value):
-        if value <= 0:
-            raise ValueError("Width must be greater than zero.")
-        self._width = value
+    s1 = Square(5)
+    print(s1)
+    print(s1.area())
+    s1.display()
 
-    @property
-    def height(self):
-        return self._height
+    print("---")
 
-    @height.setter
-    def height(self, value):
-        if value <= 0:
-            raise ValueError("Height must be greater than zero.")
-        self._height = value
+    s2 = Square(2, 2)
+    print(s2)
+    print(s2.area())
+    s2.display()
 
-    @property
-    def size(self):
-        return self.width  # or self.height, as both are the same
+    print("---")
 
-    @size.setter
-    def size(self, value):
-        self.width = value  # Set width
-        self.height = value  # Set height, ensuring the same value
-# example        
-# if __name__ == "__main__":
-
-#     s1 = Square(5)
-#     print(s1)
-#     print(s1.area())
-#     s1.display()
-
-#     print("---")
-
-#     s2 = Square(2, 2)
-#     print(s2)
-#     print(s2.area())
-#     s2.display()
-
-#     print("---")
-
-#     s3 = Square(3, 1, 3)
-#     print(s3)
-#     print(s3.area())
-#     s3.display()
+    s3 = Square(3, 1, 3)
+    print(s3)
+    print(s3.area())
+    s3.display()
