15d2cb82e10bffe2f07ae50404bff5a4325e5487
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-12-18 18:42:55 +0300

    Square

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,5 @@
 '''Importing the fuction of base from the module models and has an empty __init__ file'''
-from models.base import Base
+from base import Base
 ''''creating the class Rectangle which has inherited from Base which has been stored in models.base'''
 class Rectangle(Base):
     '''Using the init function to create various private attributes'''
