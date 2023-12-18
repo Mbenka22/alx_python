a4b552de7e7f616ebf35542008b758571c325d19
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-12-18 18:34:35 +0300

    Square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,48 +1,46 @@
 '''importing the module from models.rectangle which has various properties and methods'''
-from models.rectangle import Rectangle
+from rectangle import Rectangle
 '''the class Square which has inherited from the Rectangle class'''
 class Square(Rectangle):
     '''Initializes a Square instance.'''
     def __init__(self, size, x=0, y=0, id=None):
+        
+        ''' Initializes a Square instance.'''
+        super().__init__(size, size, x, y, id=None)
         self.size = size
         self.x = x
         self.y = y 
         self.id = id
-        '''
-        Initializes a Square instance.'''
-
         
-        
-        super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """
+        '''
         Returns a string representation of the Square instance.
 
         Returns:
         - str: A string representing the Square object in the format [Square] (<id>) <x>/<y> - <size> - in our case, width or height
-        """
+        '''
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
     
     
-    
-if __name__ == "__main__":
+# examples   
+# if __name__ == "__main__":
 
-    s1 = Square(5)
-    print(s1)
-    print(s1.area())
-    s1.display()
+#     s1 = Square(5)
+#     print(s1)
+#     print(s1.area())
+#     s1.display()
 
-    print("---")
+#     print("---")
 
-    s2 = Square(2, 2)
-    print(s2)
-    print(s2.area())
-    s2.display()
+#     s2 = Square(2, 2)
+#     print(s2)
+#     print(s2.area())
+#     s2.display()
 
-    print("---")
+#     print("---")
 
-    s3 = Square(3, 1, 3)
-    print(s3)
-    print(s3.area())
-    s3.display()
+#     s3 = Square(3, 1, 3)
+#     print(s3)
+#     print(s3.area())
+#     s3.display()
