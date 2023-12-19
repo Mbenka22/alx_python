5240876f9b8501ad2585fa5435d48aa89cafab00
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-12-19 11:40:49 +0300

    REQUESTS

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,9 @@
+import requests
+
+url = 'https://alu-intranet.hbtn.io/status'
+response = requests.get(url)
+
+# Displaying the body response in the requested format
+print("Body response:")
+print("\t- type:", type(response.text))
+print("\t- content:", response.text)
