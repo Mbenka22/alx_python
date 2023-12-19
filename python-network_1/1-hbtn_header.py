2e3a80b5a8073ed4370b95938b9eb15c94d6ee74
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-12-19 12:01:15 +0300

    Header

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,17 +1,21 @@
-'''importing requests and sys'''
+'''importing sys and requests which are inbuild/installed modules in python'''
 import requests
 import sys
 
-''' Get the URL from the command line argument'''
-url = sys.argv[1]
+def fetch_request_id(url):
+    ''' Send a GET request to the provided URL'''
+    response = requests.get(url)
 
-'''Send a GET request to the provided URL'''
-response = requests.get(url)
+    '''Check if X-Request-Id header is present in the response'''
+    if 'X-Request-Id' in response.headers:
+        request_id = response.headers['X-Request-Id']
+        print(f"The value of the X-Request-Id header is: {request_id}")
+    else:
+        print("X-Request-Id header not found in the response.")
 
-'''Check if X-Request-Id header is present in the response'''
-if 'X-Request-Id' in response.headers:
-    request_id = response.headers['X-Request-Id']
-    print(f"The value of the X-Request-Id header is: {request_id}")
-else:
-    print("X-Request-Id header not found in the response.")
+'''URL from command line argument'''
+url= sys.argv[1]
+
+'''Call the function with the provided URL'''
+fetch_request_id(url)
 
