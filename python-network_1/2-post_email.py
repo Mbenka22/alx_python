958c00fd13c610ba34abe5e2888e2154b9666a42
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-12-21 01:12:25 +0300

    Post

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -2,11 +2,12 @@
 import requests
 import sys
 
-url= input("Enter URL:")if len(sys.argv) < 2 else sys.argv[1]
+url= input("Enter URL:") if len(sys.argv) < 2 else sys.argv[1]
 email = input("Enter email :") if len(sys.argv) < 3 else sys.argv[2]
-data  = { 'email': email
+data  = { 
+    'email': email
 }
 
 """post request"""
 response = requests.post(url=url , data=data)
-print(response)
+print(response.text)
