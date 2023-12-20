c435b353b385254089857929926c388e36959531
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-12-20 13:51:16 +0300

    github

---
+++ python-network_1/6-my_github.py
@@ -0,0 +1,23 @@
+"""importing the required modules sys and requests"""
+import requests
+import sys
+
+def get_github_id(username, password):
+    url = 'https://api.github.com/user'
+    auth = (username, password)
+
+    response = requests.get(url, auth=auth)
+
+    if response.status_code == 200:
+        user_info = response.json()
+        print(f"Your GitHub ID is: {user_info['id']}")
+    else:
+        print("Failed to retrieve GitHub ID")
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        get_github_id(username, password)
+    else:
+        print("Usage: python script.py <mbenka22> <personal_access_token>")
