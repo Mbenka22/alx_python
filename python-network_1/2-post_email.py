0569033051e9f733a7f83ef1ebc242d4c92b9240
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-12-21 01:03:16 +0300

    Post

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,28 +1,11 @@
 """importing the modules requests and sys"""
 import requests
 import sys
-URL = "https://alu-intranet.hbtn.io/status"
-email = "hr@holbertonschool.com"
-def send_post_request(url, email):
-    """Create a dictionary with the email parameter"""
-    payload = {'email': "hr@holbertonschool.com"}
-    
-    """Send a POST request to the URL with the payload (email)"""
-    response = requests.post(url, data=payload)
-    
-    """Display the body of the response"""
-    print(response.text)
+url= input("Enter URL:")if len(sys.argv) < 2 else sys.argv[1]
+email = input("Enter email :") if len (sys.argv) <3 else sys.argv[2]
+data  = { 'email': email
+}
 
-if __name__ == "__main__":
-    """Check if enough arguments are passed"""
-    if len(sys.argv) < 3:
-        print("Usage: python script.py <https://alu-intranet.hbtn.io/status> <hr@holbertonschool.com")
-        sys.exit(1)
-    
-    """Extract URL and email from command line arguments"""
-    url = sys.argv[1]
-    email = sys.argv[2]
-    
-    """Call the function to send the POST request"""
-    send_post_request(url, email)
-
+"""post request"""
+response = requests.post(url=url , data=data)
+print(response)
