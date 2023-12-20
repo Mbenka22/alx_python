c554adec4341c53ed3c7ba12c5ed6e8b180e1f41
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-12-20 12:28:27 +0300

    header

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,21 +1,27 @@
-'''importing sys and requests which are inbuild/installed modules in python'''
 import requests
 import sys
 
-def fetch_request_id(url):
-    ''' Send a GET request to the provided URL'''
-    response = requests.get(url)
+url = "https://alu-intranet.hbtn.io/status"
 
-    '''Check if X-Request-Id header is present in the response'''
-    if 'X-Request-Id' in response.headers:
-        request_id = response.headers['X-Request-Id']
-        print(f"The value of the X-Request-Id header is: {request_id}")
-    else:
-        print("X-Request-Id header not found in the response.")
 
-'''URL from command line argument'''
-url= sys.argv[1]
+def get_request_id(url):
+    try:
+        response = requests.get(url)
+        
+        if response.status_code == 200:
+            request_id = response.headers.get('X-Request-Id')
+            print({request_id})
+        else:
+            print({response.status_code})
+    except requests.RequestException as e:
+        print({e})
 
-'''Call the function with the provided URL'''
-fetch_request_id(url)
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("usage:python script.py<URL>")
+        sys.exit(1)
+    
+    url = sys.argv[1]
+    get_request_id(url)
+
 
