7653f2bc8c4f5ca4b83ba40846c58893c16da792
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-12-20 13:44:35 +0300

    json

---
+++ python-network_1/5-json_api.py
@@ -0,0 +1,28 @@
+import requests
+import sys
+
+def search_user(letter):
+    if letter:
+        payload = {'q': letter}
+    else:
+        payload = {'q': ''}
+    
+    response = requests.post('http://0.0.0.0:5000/search_user', data=payload)
+    
+    try:
+        json_response = response.json()
+        if json_response:
+            print("[{}] {}".format(json_response['id'], json_response['name']))
+
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        letter = sys.argv[1]
+    else:
+        letter = ''
+    
+    search_user(letter)
