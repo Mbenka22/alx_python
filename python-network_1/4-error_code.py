e7e79da6a77dbff84f3b6b86ce18c3fbb80c1634
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-12-21 01:34:03 +0300

    Error

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -2,17 +2,12 @@
 import requests
 import sys
 url = sys.argv[1]
-def get_url_response(url):
-    response = requests.get(url)
+response = requests.get(url)
 
-    
-    """ Display the body of the response"""
+"""Check if status code is greater than or equal to 400"""
+if response.status_code >= 400:
+        print("Error code: {}" .format(response.status_code) )
+else:
     print(response.text)
-    
-    """Check if status code is greater than or equal to 400"""
-    if response.status_code >= 400:
-        print("Error code {response.status_code}" .format)
-    else:
-        print(response.status_code)
 
 
