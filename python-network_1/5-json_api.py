1c34f57445b46e0dae5e9d434e5ec44d168782fe
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-12-21 11:15:32 +0300

    Json

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -1,7 +1,12 @@
 import requests
 import sys
-url = sys.argv[1]
-def search_user(q):
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        letter = sys.argv[1]
+    else:
+        letter = ''
+
     if letter:
         payload = {'q': letter}
     else:
@@ -13,16 +18,8 @@ def search_user(q):
         json_response = response.json()
         if json_response:
             print("[{}] {}".format(json_response['id'], json_response['name']))
-
         else:
             print("No result")
     except ValueError:
         print("Not a valid JSON")
 
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        letter = sys.argv[1]
-    else:
-        letter = ''
-    
-    search_user(letter)
