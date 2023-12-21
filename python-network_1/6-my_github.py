7004e2e41ae58b793cb0dfb180b286e5e2bce682
Mimz <149058399+Mbenka22@users.noreply.github.com>
2023-12-21 11:32:48 +0300

    github

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -1,23 +1,17 @@
-"""importing the required modules sys and requests to work on the project"""
 import requests
 import sys
 
-def get_github_id(username, password):
-    url = 'https://api.github.com/user'
-    auth = (username, password)
-
-    response = requests.get(url, auth=auth)
-
-    if response.status_code == 200:
-        user_info = response.json()
-        print(f"Your GitHub ID is: {user_info['id']}")
-    else:
-        print("Failed to retrieve GitHub ID")
-
 if __name__ == "__main__":
     if len(sys.argv) == 3:
-        username = sys.argv[1]
-        password = sys.argv[2]
-        get_github_id(username, password)
+        url = 'https://api.github.com/user'
+        auth = (sys.argv[1], sys.argv[2])
+
+        response = requests.get(url, auth=auth)
+
+        if response.status_code == 200:
+            user_info = response.json()
+            print(f"Your GitHub ID is: {user_info['id']}")
+        else:
+            print("Failed to retrieve GitHub ID")
     else:
-        print("Usage: python script.py <mbenka22> <personal_access_token>")
+        print("Usage: python script.py <mbenka22> <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>")
