68a956d869b6b7cff17a40c1cc37ede02b3e5362
Mbenka22 <mbenka22@gmail.com>
2024-01-16 12:41:50 +0300

    sysy

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -4,14 +4,39 @@ Your script should connect to a MySQL server running on localhost at port 3306
 Results must be sorted in ascending order by states.id
 """
 import MySQLdb
-# from sys import argv
-# mysql_username=argv[1]
-# mysql_password=argv[2]
-# db_name=argv[3]
+import sys
 
-connect= MySQLdb.connect(host='localhost', user='mirriam', passwd='florence2024#', db='hbtn_0e_0_usa', port=3006)
-cursor=connect.cursor()
-create_table_query=("CREATE TABLE IF NOT EXISTS states (id  INT NOT NULL AUTO-INCREMENT), name VARCHAR(256) NOT NULL, PRIMARY KEY(id)")
-cursor.execute(create_table_query)
-insert_data_query = "INSERT INTO states (name) VALUES (%s),(%s),(%s),(%s),(%s)"
-data_values = ('California', 'Arizona', 'Texas', 'New York', 'Nevada')
+# Check if correct number of arguments is provided
+if len(sys.argv) != 4:
+    print("Usage: python script.py <mysql_username> <mysql_password> <database_name>")
+    sys.exit(1)
+
+# Retrieve command line arguments
+mysql_username = sys.argv[1]
+mysql_password = sys.argv[2]
+database_name = sys.argv[3]
+
+# Establish a connection to MySQL server
+conn = MySQLdb.connect(host="localhost", port=3306, user=mysql_username, passwd=mysql_password, db=database_name)
+
+# Create a cursor
+cursor = conn.cursor()
+
+# Execute SQL query to select states starting with 'N'
+sql_query = """
+    SELECT * FROM states
+    WHERE name LIKE 'N%'
+    ORDER BY states.id;
+"""
+
+# Execute the query
+cursor.execute(sql_query)
+
+# Fetch and display the results
+results = cursor.fetchall()
+for row in results:
+    print(row)
+
+# Close the cursor and connection
+cursor.close()
+conn.close()
