15b13d490c9caba832937797b3067c3aa1eaf28f
Mbenka22 <mbenka22@gmail.com>
2024-01-17 11:37:17 +0300

    injection

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,18 @@
+"Trunctate"
+import MySQLdb
+from sys import argv
+mysql_username=argv(1)
+mysql_password=argv(2)
+db_name=argv(4)
+
+connect=MySQLdb.connect(
+    host='localhost',
+    user=mysql_username,
+    passwd=mysql_password, 
+    database=db_name, 
+    port=3006)
+
+cursor=connect.cursor()
+query="Arizona'; TRUNCATE TABLE states ; SELECT * FROM states WHERE name = '"
+cursor.execute(query)
+
