0fb413809e2a468802906de1487e01d999dc0ac3
Mbenka22 <mbenka22@gmail.com>
2024-01-18 00:11:48 +0300

    FORMAT

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,19 @@
-"""a script that takes in an arguement and dispalys all the values in the states table"""
-import mySQLdb
+import MySQLdb
 from sys import argv
-mysql_username=argv(1)
-mysql_password=argv(2)
-db_name=argv(3)
-state_name=argv(4)
-if __name__=='__main__':
-    connect=mySQLdb.connect(host='localhost', user=mysql_username, passwd=mysql_password, databse=db_name,port=3006)
-    cursor=connect.cursor()
-    sql_query="SELECT *FROM states WHERE name = %s ORDER BY id ASC"
-    cursor.execute(sql_query(state_name,))
-    results=cursor.fetchall()
-    for row in results:
+
+if __name__ == '__main__':
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    db_name = argv[3]
+    # statename_searched = input("enter state name:  ")
+    
+
+    connect = MySQLdb.connect(host='localhost', user=mysql_username, passwd=mysql_password, port=3006, db=db_name)
+    cursor = connect.cursor()
+    sql_query = "SELECT * FROM states WHERE name LIKE BINARY '{}'".format(argv[4])
+    cursor.execute(sql_query)
+    rows = cursor.fetchall()
+    for row in rows:
         print(row)
     cursor.close()
-    connect.close()    
+    connect.close()
