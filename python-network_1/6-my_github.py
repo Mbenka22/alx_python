ec45fb0adb90c0b8e87bad7bc8758de287e1a2a4
Mbenka22 <mbenka22@gmail.com>
2024-01-18 00:20:29 +0300

    github

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -1,14 +1,21 @@
+'''import request and sys '''
 import requests
 from requests.auth import HTTPBasicAuth
 import sys
+
+
 username = sys.argv[1]
 access_token = sys.argv[2]
-response = requests.get("https://github.com/Mbenka22" ,auth = HTTPBasicAuth(username , access_token))
+
+response = requests.get("https://api.github.com/Mbenka22", 
+                        auth=HTTPBasicAuth(username, access_token))
+
+
+
 try:
-    if response.status_code == 200:
-        user_data = response.json()
-        print(user_data['id'])
-    else:
-        print("None")
-except ValueError:
-    print("None")            
+   if response.status_code == 200:
+    user_data = response.json()
+    print(user_data['id'])
+   else:
+    print("None")
+except ValueError:        
