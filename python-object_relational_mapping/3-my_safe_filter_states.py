d89d8f71a7cf077b983243b83116c67625edaa3c
Mbenka22 <mbenka22@gmail.com>
2024-01-18 00:14:36 +0300

    truncate

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,23 +1,21 @@
 "Trunctate"
 import MySQLdb
 from sys import argv
-mysql_username=argv(1)
-mysql_password=argv(2)
-db_name=argv(3)
 
-connect=MySQLdb.connect(
-    host='localhost',
-    user=mysql_username,
-    passwd=mysql_password, 
-    database=db_name, 
-    port=3006)
-
-cursor=connect.cursor()
-query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+if __name__ == '__main__':
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    db_name = argv[3]
+    # statename_searched = input("enter state name:  ")
     
-cursor.execute(query)
 
-
-cursor.close
-connect.close
+    connect = MySQLdb.connect(host='localhost', user=mysql_username, passwd=mysql_password, port=3006, db=db_name)
+    cursor = connect.cursor()
+    sql_query = "SELECT * FROM states WHERE name LIKE BINARY %s"
+    cursor.execute(sql_query, (argv[4],))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    connect.close()
 
