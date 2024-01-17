f96e3dd50bb5f2e7f2d94f7d4ba5bb7096f11003
Mbenka22 <mbenka22@gmail.com>
2024-01-18 00:18:18 +0300

    inner join

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,25 +1,24 @@
+"""inner join"""
 import MySQLdb
 from sys import argv
-mysql_username=argv(1)
-mysql_password=argv(2)
-db_name=argv(3)
 
-connect=MySQLdb.connect(
-    host='localhost',
-    user=mysql_username,
-    passwd=mysql_password, 
-    database=db_name, 
-    port=3006)
+if __name__ == '__main__':
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    db_name = argv[3]
+    # statename_searched = input("enter state name:  ")
+    
 
-cursor=connect.cursor()
-cursor = connect.cursor()
-cursor.execute(
-        """SELECT cities.id, cities.name, states.name
-        FROM cities
-        INNER JOIN states ON cities.state_id=states.id
-        ORDER BY cities.id ASC;
-        """)
-for row in cursor:
-    print(row)
-cursor.close()
-connect.close()
+    connect = MySQLdb.connect(host='localhost', user=mysql_username, passwd=mysql_password, port=3306, db=db_name)
+    cursor = connect.cursor()
+    sql_query = """SELECT cities.id,cities.name, states.name
+               FROM cities
+               INNER JOIN states ON
+               cities.state_id=states.id
+                ORDER BY cities.id"""
+    cursor.execute(sql_query)
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    connect.close()
