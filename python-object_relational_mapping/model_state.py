91802b0adeff3cc35e7469ae8cd8c4f884b2995d
Mbenka22 <mbenka22@gmail.com>
2024-01-18 22:32:45 +0300

    bind

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,3 +1,4 @@
+"""importing various models """
 from sqlalchemy import create_engine, Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
