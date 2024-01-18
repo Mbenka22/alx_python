e43b22db992600648c8878d0c4dc581415f2519a
Mbenka22 <mbenka22@gmail.com>
2024-01-18 13:24:47 +0300

    chess

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,5 +1,29 @@
-from sqlalchemy import create_engine, ForeignKey,column, String,INTEGER,CHAR
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+# from sqlalchemy import create_engine, ForeignKey,column, String,INTEGER,CHAR
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
 
-Base = declarative_base()
+# Base = declarative_base()
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    mysql_username = argv[1]
+    mysql_passwd = argv[2]
+    db_name = argv[3]
+    state_name = argv[4]
+
+    conn = MySQLdb.connect(host='localhost', user=mysql_username, passwd=mysql_passwd, port=3306, db=db_name)
+    cursor = conn.cursor()
+    query = """SELECT cities.name
+               FROM cities
+               INNER JOIN states ON
+               cities.state_id=states.id
+               WHERE states.name = %s
+               ORDER BY cities.id ASC
+                """
+    cursor.execute(query, (state_name,))
+    rows = cursor.fetchall()
+    cities = [row[0] for row in rows]
+    print(", ".join(cities))
+    cursor.close()
+    conn.close()
