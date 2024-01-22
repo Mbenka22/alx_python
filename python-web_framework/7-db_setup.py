832365f8f1334418b55ae45ef6690fc375a96dbd
Mbenka22 <mbenka22@gmail.com>
2024-01-22 12:05:09 +0300

    flask

--- python-web_framework/7-db_setup.py
+++ python-web_framework/7-db_setup.py
@@ -14,3 +14,35 @@ db_name = sys.argv[3]
 db_host = 'localhost'
 
 app = Flask(__name__)
+
+############################# TO DO 1 ############################
+# Add your code to connect to the database here
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{db_username}:{db_password}@{db_host}/{db_name}'
+db = SQLAlchemy(app)
+###############################################################
+
+############################ TO DO 2 ##############################
+# Define your USER Model class here
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+#################################################################
+
+# Create the database tables
+def create_tables():
+    with app.app_context():
+        db.create_all()
+
+create_tables()  # This calls the function to create tables
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    return "Hello, ALX Flask!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
