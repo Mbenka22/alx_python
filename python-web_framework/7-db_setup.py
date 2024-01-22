f4184f8659d5c5b7807b27e4cf3714922379a840
Mbenka22 <mbenka22@gmail.com>
2024-01-22 11:48:40 +0300

    FLASK

---
+++ python-web_framework/7-db_setup.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, render_template, flash, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import re
+import sys
+
+# Check for command-line arguments
+if len(sys.argv) != 4:
+    print("Usage: python 8-add_retrieve_users.py <db_username> <db_password> <db_name>")
+    sys.exit(1)
+
+db_username = sys.argv[1]
+db_password = sys.argv[2]
+db_name = sys.argv[3]
+db_host = 'localhost'
+
+app = Flask(__name__)
