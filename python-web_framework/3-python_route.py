f1dd3d04194b9bff7e105e58c790f1c45515f577
Mbenka22 <mbenka22@gmail.com>
2024-01-22 22:13:58 +0300

    Default parameters

---
+++ python-web_framework/3-python_route.py
@@ -0,0 +1,27 @@
+"""Basic flask web server using flask"""
+from flask import Flask
+
+app = Flask(__name__)
+#home
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+#first route within a route
+@app.route('/hbnb',strict_slashes=False)
+def hbnb():
+    return "HBNB"
+#dynamic routes
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    # Replace underscores with spaces in the text variable
+    formatted_text = text.replace('_', ' ')
+    return f"C {formatted_text}"
+#python parameters
+@app.route('/python/<text>',strict_slashes=False)
+def python_route(text):
+    formatted_text=text.replace('_','')
+    return f"python{formatted_text}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000,debug=True)
