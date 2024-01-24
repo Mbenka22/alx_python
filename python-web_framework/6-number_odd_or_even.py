bcc683c8517daa8a1a77218cbbab02ab71d8a1b3
Mbenka22 <mbenka22@gmail.com>
2024-01-24 12:47:05 +0300

    number

--- python-web_framework/6-number_odd_or_even.py
+++ python-web_framework/6-number_odd_or_even.py
@@ -38,10 +38,10 @@ def display_number_template(n):
         return render_template('5-number.html', n=n)
 
 # /number_odd_or_even/<n>
-@app.route('/number_odd_or_even/<n>',strict_slashes=False)
+@app.route('/number_odd_or_even/<int:numbrn>',strict_slashes=False)
 def number_odd_or_even(n):
     result = "even" if n % 2 == 0 else "odd"
-    return "6-number_odd_or_even.html",number=n,result=result
+    return render_template('6-number_odd_or_even.html', number=n, result=result)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000,debug=True)
