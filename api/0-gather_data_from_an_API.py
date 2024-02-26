8371182202489b00fe1d738e0cd3b1d98a95d36d
Mbenka22 <mbenka22@gmail.com>
2024-02-26 11:44:40 +0300

    API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -12,14 +12,14 @@ def get_employee_todo_progress(employee_id):
     total_tasks = len(todo_data)
     done_tasks = sum(1 for todo in todo_data if todo['completed'])
 
-    print(f"Employee {employee_name} is done with tasks ({done_tasks}/{total_tasks}):")
+    print(f" {employee_name} is done with tasks ({done_tasks}/{total_tasks}):")
     for todo in todo_data:
         if todo['completed']:
             print(f"\t{todo['title']}")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python script.py EMPLOYEE_ID")
+        print("Usage: EMPLOYEE_ID")
         sys.exit(1)
     
     employee_id = int(sys.argv[1])
