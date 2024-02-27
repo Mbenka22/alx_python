e4d0f0b20c1ccfcd7a59c4fdc89213597e5a552d
Mbenka22 <mbenka22@gmail.com>
2024-02-27 12:05:43 +0300

    api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,42 +1,38 @@
 import requests
 import sys
-import json
 
 def employee_info(employee_id):
-    """Getting employee details from the given URL by appending the employee_id to the given URL."""
+    """ getting employee details from the given url by appending the employee_id  to the given url"""
     employee_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
     employee_response = requests.get(employee_url)
     employee_data = employee_response.json()
 
-    """Getting employee's todos by appending the todo path to the URL."""
+    """getting employee's todo's by appending the todo path the url"""
     todos_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
     todos_response = requests.get(todos_url)
     todos_data = todos_response.json()
 
-    """Filtering completed tasks."""
-    completed_tasks = [{'task': task['title'], 'completed': task['completed'], 'username': employee_data['username']} for task in todos_data]
+ 
+    completed_tasks = [task for task in todos_data if task['completed']]
+    number_of_done_tasks = len(completed_tasks)
+    total_number_of_tasks = len(todos_data)
 
-    """Creating the dictionary for JSON output."""
-    output_data = {str(employee_id): completed_tasks}
+    """Display the employee's name and the number of done and total tasks."""
+    print(f"Employee {employee_data['name']} is done with tasks({number_of_done_tasks}/{total_number_of_tasks}):")
 
-    """Writing to JSON file."""
-    with open(f"{employee_id}.json", "w") as json_file:
-        json.dump(output_data, json_file, indent=4)
-
-    """Displaying the employee's name and the number of done and total tasks."""
-    print(f"Employee {employee_data['name']} is done with tasks({len(completed_tasks)}/{len(todos_data)}):")
-    
-    """Displaying done tasks."""
+    """Done tasks."""
     for task in completed_tasks:
-        print(f"\t{task['task']}")
+        print(f"\t {task['title']}")
+
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python script_name.py employee_id.")
         sys.exit(1)
 
-    """Employee ID from the second argument."""
+    """employee id from the second argument"""
     employee_id = int(sys.argv[1])
 
-    """Calling the function."""
+    """calling the function"""
     employee_info(employee_id)
