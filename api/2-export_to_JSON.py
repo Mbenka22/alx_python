359cbb0627ef154c4eed8d892575a0d2f393fed6
Mbenka22 <mbenka22@gmail.com>
2024-02-27 11:36:29 +0300

    json

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,38 +1,32 @@
-import json
 import requests
 import sys
-
+import json
 
 def employee_info(employee_id):
-    """ getting employee details from the given url by appending the employee_id  to the given url"""
+    # Getting employee details
     employee_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
     employee_response = requests.get(employee_url)
-    employee_data = json.loads(employee_response.text)
+    employee_data = employee_response.json()
 
-    """getting employee's todo's by appending the todo path the url"""
+    # Getting employee's todos
     todos_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
     todos_response = requests.get(todos_url)
-    todos_data = json.loads(todos_response.text)
+    todos_data = todos_response.json()
 
-    completed_tasks = [task for task in todos_data if task['completed']]
-    number_of_done_tasks = len(completed_tasks)
-    total_number_of_tasks = len(todos_data)
+    # Filter completed tasks
+    completed_tasks = [{'task': task['title'], 'completed': task['completed'], 'username': employee_data['username']} for task in todos_data]
 
-    """Display the employee's name and the number of done and total tasks."""
-    print(f"Employee {employee_data['name']} is done with tasks({number_of_done_tasks}/{total_number_of_tasks}):")
+    # Creating the dictionary for JSON output
+    output_data = {str(employee_id): completed_tasks}
 
-    """Done tasks."""
-    for task in completed_tasks:
-        print(f"\t {task['title']}")
-
+    # Writing to JSON file
+    with open(f"{employee_id}.json", "w") as json_file:
+        json.dump(output_data, json_file, indent=4)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python script_name.py employee_id.")
         sys.exit(1)
 
-    """employee id from the second argument"""
     employee_id = int(sys.argv[1])
-
-    """calling the function"""
     employee_info(employee_id)
